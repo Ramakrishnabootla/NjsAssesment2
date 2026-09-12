331439bcd1438d7666f55b2d14a75355b2695118
Ramakrishna <bootlaramakrishna0@gmail.com>
2026-09-12 15:09:56 +0530

    updated

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const path = require('path');
-const employeeRoutes = require('./routes/employeeRoutes');
 
 const app = express();
-app.use(express.json());
-
-app.use('/api/employees', employeeRoutes);
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
